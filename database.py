55b2606b43768fe5137a1f269ab3c83def0ce6d0
alexandros173 <claudio.cardozo@gmail.com>
2023-10-27 13:53:00 -0400

    Now using DB_URL env variable to connect to Postgres in Heroku

--- database.py
+++ database.py
@@ -7,12 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-
-engine = create_engine(f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}?sslmode=require')
+engine = create_engine(f'{os.getenv("DB_URL")}?sslmode=require')
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
